5e2b44605c6ea4eed9eaf8ccd829ecba89d7ff8b
Kranti <darkphoton20@gmail.com>
2024-07-01 20:35:04 +0800

    Updating json structure and refactoring server.

--- internal/product/model.go
+++ internal/product/model.go
@@ -1,8 +1,8 @@
 package product
 
 type Product struct {
-	ID       int
-	Title    string
-	Subtitle string
-	ImageUrl string
+	ID       int    `json:"id"`
+	Title    string `json:"title"`
+	Subtitle string `json:"subtitle"`
+	ImageUrl string `json:"imageUrl"`
 }
